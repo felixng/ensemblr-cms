2ec696b1e4a42622726b0e0f4c4ba337f25ed7db
felix.ng <nfelix@gmail.com>
2017-07-15 22:02:23 +0100

    initial version

---
+++ models/Production.js
@@ -0,0 +1,67 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+/**
+ * Production Model
+ * =============
+ */
+
+var Production = new keystone.List('Production', {
+	singular: 'Production',
+	plural: 'Productions',
+	map: { name: 'name' },
+    autokey: { path: 'slug', from: 'name', unique: true },
+    defaultSort: '-lastUpdated',
+});
+
+Production.add({
+	name: { type: String, required: true },
+	genre: { type: String },
+	showTime: { type: String },
+	duration: { type: String, },
+	previewFrom: { type: Date, },
+	openingNight: { type: Date, },
+	twitter: { type: String, width: 'small' },
+	facebook: { type: String, width: 'small' },
+	confirmedClosing: { type: Boolean, width: 'small', index: true },
+	createdAt: { type: Date, default: Date.now },
+	lastModified: { type: Date, default: Date.now },
+});
+
+// Production.schema.pre('save', function (next) {
+// 	this.wasNew = this.isNew;
+// 	next();
+// });
+
+// Production.schema.post('save', function () {
+// 	if (this.wasNew) {
+// 		this.sendNotificationEmail();
+// 	}
+// });
+
+// Production.schema.methods.sendNotificationEmail = function (callback) {
+// 	if (typeof callback !== 'function') {
+// 		callback = function () {};
+// 	}
+// 	var Production = this;
+// 	keystone.list('User').model.find().where('isAdmin', true).exec(function (err, admins) {
+// 		if (err) return callback(err);
+// 		new keystone.Email({
+// 			templateExt: 'hbs',
+// 			templateEngine: require('express-handlebars'),
+// 			templateName: 'production-notification',
+// 		}).send({
+// 			to: admins,
+// 			from: {
+// 				name: 'Ensemblr',
+// 				email: 'contact@ensemblr.co',
+// 			},
+// 			subject: 'New Production for Ensemblr',
+// 			Production: Production,
+// 		}, callback);
+// 	});
+// };
+
+Production.defaultSort = '-createdAt';
+Production.defaultColumns = 'name, productionType, createdAt';
+Production.register();
---
+++ routes/api.js
@@ -0,0 +1,36 @@
+var keystone = require('keystone');
+var _ = require('lodash')
+var express = keystone.express;
+var router = express.Router();
+
+// var Production = require('../models/Production');
+var Production = keystone.list('Production');
+router.route('/productions').get(function(req, res) {
+    Production.model.find().exec(function(err, docs) {
+        if (err) res.send(err);
+        res.json(docs);
+    });
+});
+
+router.route('/productions/twitters').get(function(req, res) {
+    Production.model.find().where('twitter').ne(null).exec(function(err, docs) {
+        if (err) res.send(err);
+
+        var handles = docs.map(function(doc) {
+        	return doc.twitter.replace('https://twitter.com/', '');
+        });
+
+        res.json(_.uniq(handles));
+    });
+});
+
+router.route('/productions/twitter/:handle').get(function(req, res) {
+    Production.model.find().where('twitter', req.param.handle).exec(function(err, docs) {
+        if (err) res.send(err);
+        res.json(docs);
+    });
+});
+
+module.exports = {
+	router: router
+}
